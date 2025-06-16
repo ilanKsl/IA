d206aed3baa45c54e44701fa5d4aec799274d0d1
Dollyn <151972181+ilanKsl@users.noreply.github.com>
2025-06-16 13:24:19 -0300

    1

--- missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
+++ missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual desses vilões você prefere",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Diavolo",
+                afirmacao: "PSICOPATA!!"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Dio",
+                afirmacao: "Eu sei o porque você quer parar o tempo."
             }
         ]
     },
